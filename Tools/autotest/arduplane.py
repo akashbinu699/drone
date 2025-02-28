tompoch2/ardupilot

Tools: support opaque IDs for current mission

Tools/autotest/arduplane.py
@@ -4117,6 +4117,48 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
         attempt_fence_breached_disable(start_mode="FBWA", end_mode="FBWA", expected_mode="GUIDED", action=6)
         attempt_fence_breached_disable(start_mode="FBWA", end_mode="FBWA", expected_mode="GUIDED", action=7)
 
+    def get_mission_checksum(self):
+        self.drain_mav()
+        m = self.poll_message('MISSION_CURRENT')
+        print("Got %s" % str(m))
+        return m.mission_id
+
+    def MissionOpaqueID(self):
+        '''test support for vehicle identifying mission via opaque ID'''
+        jumpcount_checksum = 2006072681
+        for missionstuff in [("text.txt", 4264322242),
+                             ("empty.txt", 4294967295),
+                             ("justhome.txt", 4294967295),
+                             ("jumpcount.txt", jumpcount_checksum),
+                             ]:
+            (filename, expected_checksum) = missionstuff
+            self.progress("Checking checksum for (%s)" % (filename,))
+            self.load_mission(filename)
+            self.assert_cached_message_field_values('MISSION_ACK', {
+                "opaque_id": expected_checksum,
+            })
+            # ensure it appears in the MISSION_CURRENT message:
+            self.assert_received_message_field_values('MISSION_CURRENT', {
+                "mission_id": expected_checksum,
+            }, poll=True)
+
+        # fly the jump count mission, make sure the checksum doesn't
+        # change over time
+        self.load_mission("jumpcount.txt", strict=False)
+        self.set_current_waypoint(0, check_afterwards=False)
+        self.change_mode('AUTO')
+        self.wait_ready_to_arm()
+        self.arm_vehicle()
+        for i in range(2):
+            self.wait_current_waypoint(2, timeout=120)
+            self.wait_current_waypoint(3, timeout=120)
+            mission_checksum = self.get_mission_checksum()
+            if mission_checksum != jumpcount_checksum:
+                raise NotAchievedException(
+                    "Mission checksum changed during mission; got=%u want=%u i=%u" %
+                    (jumpcount_checksum, mission_checksum, i))
+        self.fly_home_land_and_disarm()
+
     def _MAV_CMD_DO_AUX_FUNCTION(self, run_cmd):
         '''Test triggering Auxiliary Functions via mavlink'''
         self.context_collect('STATUSTEXT')
@@ -6145,6 +6187,7 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
             self.AutotuneFiltering,
             self.MegaSquirt,
             self.Hirth,
+            self.MissionOpaqueID,
             self.MSP_DJI,
             self.SpeedToFly,
             self.GlideSlopeThresh,

akashbinu699/drone

Tools/autotest/arduplane.py
@@ -4164,6 +4164,47 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
         attempt_fence_breached_disable(start_mode="FBWA", end_mode="FBWA", expected_mode="GUIDED", action=6)
         attempt_fence_breached_disable(start_mode="FBWA", end_mode="FBWA", expected_mode="GUIDED", action=7)
 
+    def get_mission_checksum(self):
+        self.drain_mav()
+        m = self.poll_message('MISSION_CURRENT')
+        print("Got %s" % str(m))
+        return m.mission_id
+
+    def MissionOpaqueID(self):
+        '''test support for vehicle identifying mission via opaque ID'''
+        jumpcount_checksum = 970034527
+        for missionstuff in [("text.txt", 4264322242),
+                             ("justhome.txt", 4294967295),
+                             ("jumpcount.txt", jumpcount_checksum),
+                             ]:
+            (filename, expected_checksum) = missionstuff
+            self.start_subtest("Checking checksum for (%s)" % (filename,))
+            self.load_mission(filename)
+            self.assert_cached_message_field_values('MISSION_ACK', {
+                "opaque_id": expected_checksum,
+            })
+            # ensure it appears in the MISSION_CURRENT message:
+            self.assert_received_message_field_values('MISSION_CURRENT', {
+                "mission_id": expected_checksum,
+            }, poll=True)
+
+        # fly the jump count mission, make sure the checksum doesn't
+        # change over time
+        self.load_mission("jumpcount.txt", strict=False)
+        self.set_current_waypoint(0, check_afterwards=False)
+        self.change_mode('AUTO')
+        self.wait_ready_to_arm()
+        self.arm_vehicle()
+        for i in range(2):
+            self.wait_current_waypoint(2, timeout=120)
+            self.wait_current_waypoint(3, timeout=120)
+            mission_checksum = self.get_mission_checksum()
+            if mission_checksum != jumpcount_checksum:
+                raise NotAchievedException(
+                    "Mission checksum changed during mission; got=%u want=%u i=%u" %
+                    (jumpcount_checksum, mission_checksum, i))
+        self.fly_home_land_and_disarm()
+
     def _MAV_CMD_DO_AUX_FUNCTION(self, run_cmd):
         '''Test triggering Auxiliary Functions via mavlink'''
         self.context_collect('STATUSTEXT')
@@ -6907,6 +6948,7 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
             self.AutotuneFiltering,
             self.MegaSquirt,
             self.Hirth,
+            self.MissionOpaqueID,
             self.MSP_DJI,
             self.SpeedToFly,
             self.GlideSlopeThresh,