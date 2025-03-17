knalam2/ardupilot

Plane: Relax target airspeed constraints
Relaxed target airspeed constraints in navigation.cpp to allow for
target airspeeds as low as the AIRSPEED_STALL if set. The TECS will
still further constrain the target airspeed as appropriate.

ArduPlane/navigation.cpp
@@ -267,8 +267,12 @@ void Plane::calc_airspeed_errors()
         target_airspeed_cm += airspeed_nudge_cm;
     }
 
+    int32_t airspeed_lower_bound = is_positive(aparm.airspeed_stall)
+                                       ? aparm.airspeed_stall
+                                       : aparm.airspeed_min;
+
     // Apply airspeed limit
-    target_airspeed_cm = constrain_int32(target_airspeed_cm, aparm.airspeed_min*100, aparm.airspeed_max*100);
+    target_airspeed_cm = constrain_int32(target_airspeed_cm, airspeed_lower_bound*100, aparm.airspeed_max*100);
 
     // use the TECS view of the target airspeed for reporting, to take
     // account of the landing speed

akashbinu699/drone

ArduPlane/navigation.cpp
@@ -267,8 +267,12 @@ void Plane::calc_airspeed_errors()
         target_airspeed_cm += airspeed_nudge_cm;
     }
 
+    float airspeed_lower_bound = is_positive(aparm.airspeed_stall)
+                                     ? aparm.airspeed_stall
+                                     : aparm.airspeed_min;
+
     // Apply airspeed limit
-    target_airspeed_cm = constrain_int32(target_airspeed_cm, aparm.airspeed_min*100, aparm.airspeed_max*100);
+    target_airspeed_cm = constrain_int32(target_airspeed_cm, airspeed_lower_bound*100, aparm.airspeed_max*100);
 
     // use the TECS view of the target airspeed for reporting, to take
     // account of the landing speed